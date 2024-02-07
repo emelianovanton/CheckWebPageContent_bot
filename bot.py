066ca468b41cfc01724faa7e5f536024ac8bd66e
aemelyanov-ms <100220985+aemelyanov-ms@users.noreply.github.com>
2024-02-07 10:07:48 +0200

    change users id to channel id

--- bot.py
+++ bot.py
@@ -1,40 +1,38 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
-import os
+from telegram.ext import Updater, CommandHandler, CallbackContext, Job, MessageHandler, Filters
 
 url_to_check = os.environ.get('WEBPAGE_URL')
 bot_token = os.environ.get('BOT_TOKEN')
+channel_id = os.environ.get('CHANNEL_ID')
 previous_content = ''
 user_ids = set()
 
 
 def start(update: Update, context: CallbackContext) -> None:
-    user_id = update.message.chat_id
-    user_ids.add(user_id)
-    update.message.reply_text('Bot is running. Use /check to get the web page content or /runcheck to manually check.')
+    chat_id = update.message.chat_id
+    context.bot.send_message(chat_id,
+                             'Bot is running. Use /check to get the web page content or /runcheck to manually check.')
 
 
 def check(context: CallbackContext) -> None:
     global previous_content
 
     try:
-        response = requests.get(str(url_to_check))
+        response = requests.get(url_to_check)
         soup = BeautifulSoup(response.text, 'html.parser')
         current_content = str(soup)
 
         if current_content != previous_content:
-            for user_id in user_ids:
-                context.bot.send_message(user_id, 'Web page content changed!\n\n' + current_content)
+            context.bot.send_message(channel_id, 'Web page content changed!\n\n' + current_content)
             previous_content = current_content
         else:
-            for user_id in user_ids:
-                context.bot.send_message(user_id, 'Web page content remains the same.')
+            context.bot.send_message(channel_id, 'Web page content remains the same.')
 
     except Exception as e:
-        for user_id in user_ids:
-            context.bot.send_message(user_id, f'Error: {str(e)}')
+        context.bot.send_message(channel_id, f'Error: {str(e)}')
 
 
 def run_check(update: Update, context: CallbackContext) -> None:
@@ -47,9 +45,10 @@ def main() -> None:
 
     updater.dispatcher.add_handler(CommandHandler("start", start))
     updater.dispatcher.add_handler(CommandHandler("runcheck", run_check))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, run_check))
 
-    # Schedule the check job every 2 minutes
-    job_queue.run_repeating(check, interval=120, first=0)
+    # Schedule the check job every 5 minutes
+    job_queue.run_repeating(check, interval=300, first=0)
 
     updater.start_polling()
 
